7f4046e5ea780c0bee6db4a257156dfb0f822a13
soukouki <soukouki0@yahoo.co.jp>
2024-07-03 15:34:49 +0900

    HoT: initial revision

---
+++ hot.rb
@@ -0,0 +1,14 @@
+
+puts "Tossing a coin..."
+heads = 0
+tails = 0
+3.times do |i|
+  if rand(2) == 0
+    puts "Round #{i + 1}: Heads"
+    heads += 1
+  else
+    puts "Round #{i + 1}: Tails"
+    tails += 1
+  end
+end
+puts "Heads: #{heads}, Tails: #{tails}"
